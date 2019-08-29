66fdbc594a6eb9e688bd9c138c6e08caa8edfc48
NachoVai <reyes.s.ign@gmail.com>
2019-08-29 14:55:57 -0400

    comments

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -1,4 +1,4 @@
-var dates = {
+var data = {
   token: document.getElementsByName('csrf-token')[0].content,
   product: null,
   btn: null,
@@ -12,33 +12,33 @@ var dates = {
 
 var actions = {
   addCart (e){
-    dates.product = e.getAttribute("product");
-    dates.btn = e;
+    data.product = e.getAttribute("product");
+    data.btn = e;
     fetch('/my_shopping_carts/',
       {
         method: 'POST',
         headers: {           
-          'X-CSRF-Token': dates.token,
+          'X-CSRF-Token': data.token,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body:
           JSON.stringify(
             {
-              "product_id":dates.product
+              "product_id":data.product
             }
           )
       }
     )
     .then( response => {
-      dates.status = response.clone().status
+      data.status = response.clone().status
       return response.json()
     })
     .then( response => {
-      dates.response = response 
-      if(dates.status == 200){
+      data.response = response 
+      if(data.status == 200){
         document.getElementById('mycart').textContent = response.cart;
-        ap_accions.toast_alert("success", dates.response.response, 2000)
+        ap_accions.toast_alert("success", data.response.response, 2000)
       } else {
         ap_accions.toast_alert("error", response.products[0] , 2000)
       }
@@ -46,68 +46,68 @@ var actions = {
     .catch(console.log)
   },
   addFav (e){
-    dates.product = e.getAttribute("product");
-    dates.btn = e;
-    fetch('/favorites/',
+    data.product = e.getAttribute("product");
+    data.btn = e;
+    fetch('/favodata/',
       {
         method: 'POST',
         headers:{
-          'X-CSRF-Token': dates.token,
+          'X-CSRF-Token': data.token,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(
           {
-            "product_id":dates.product 
+            "product_id":data.product 
           }
         )
       },
     ).then( result => {
-      dates.status = result.clone().status // 200 o 400
+      data.status = result.clone().status // 200 o 400
       return result.json()
     })
     .then( result => {
-      dates.result = result 
-      if(dates.status == 200){
-        ap_accions.toast_alert("success", dates.result.response, 2000)
+      data.result = result 
+      if(data.status == 200){
+        ap_accions.toast_alert("success", data.result.response, 2000)
       } else {
-        ap_accions.toast_alert("error", result.favorites[0], 2000)
+        ap_accions.toast_alert("error", result.favodata[0], 2000)
       }
     })
     .catch(console.log)
   },
   deleteItem(e,fila){
-    dates.total   = parseInt(document.getElementById('mycartTotal').textContent)
-    dates.product = parseInt(e.getAttribute("product"))
-    dates.element = document.getElementById("p_" + fila)
+    data.total   = parseInt(document.getElementById('mycartTotal').textContent)
+    data.product = parseInt(e.getAttribute("product"))
+    data.element = document.getElementById("p_" + fila)
     fetch('/my_shopping_carts/' + fila,
       {
         method: 'DELETE',
         headers: {           
-          'X-CSRF-Token': dates.token,
+          'X-CSRF-Token': data.token,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body:
           JSON.stringify(
             {
-              "cart_id":dates.cart_id
+              "cart_id":data.cart_id
             }
           )
       }
     )
     .then( response => {
-      dates.status = response.clone().status
+      data.status = response.clone().status
       return response.json()
     })
     .then( response => {
-      if(dates.status == 200){
-        dates.product = response.product
+      if(data.status == 200){
+        data.product = response.product
         if (response.update_cart == 0)
-          dates.element.style.display = "none"        
+          data.element.style.display = "none"        
         else
-          dates.element.children[2].textContent = response.update_cart
-        document.getElementById('mycartTotal').textContent = parseFloat(dates.total - dates.product.p_price)
+          data.element.children[2].textContent = response.update_cart
+        document.getElementById('mycartTotal').textContent = parseFloat(data.total - data.product.p_price)
         document.getElementById('mycart').textContent = response.cart;
         ap_accions.toast_alert("success", response.response, 500)
       } else {
@@ -118,26 +118,26 @@ var actions = {
 
   },
   deleteFavorite(e,fila){
-    dates.product = parseInt(e.getAttribute("product"))
-    dates.element = document.getElementById("p_" + fila)
-    fetch('/favorites/' + fila,
+    data.product = parseInt(e.getAttribute("product"))
+    data.element = document.getElementById("p_" + fila)
+    fetch('/favodata/' + fila,
       {
         method: 'DELETE',
         headers: {           
-          'X-CSRF-Token': dates.token,
+          'X-CSRF-Token': data.token,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         }
       }
     )
     .then( response => {
-      dates.status = response.clone().status
+      data.status = response.clone().status
       return response.json()
     })
     .then( result => {
-      if(dates.status == 200){
+      if(data.status == 200){
         ap_accions.toast_alert("success", result.response, 1000)
-        dates.element.style.display = "none"
+        data.element.style.display = "none"
       } else {
         ap_accions.toast_alert("error", result.products[0], 1000)        
       }
@@ -147,5 +147,5 @@ var actions = {
 }
 
 $(document).ready(function(){
-  dates.cart = parseInt(document.getElementById("mycart").textContent)
+  data.cart = parseInt(document.getElementById("mycart").textContent)
 })
